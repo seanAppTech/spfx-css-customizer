ef1cc718b19aefb4d1e1b3f824266707b912b7ba
Sean Standen <SStanden@Adm2s-MBP.AppTechcorp.com>
2022-08-09 12:44:23 -0700

    Initialized project.

---
+++ src/extensions/cssExtension/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ICssExtensionApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'CssExtensionApplicationCustomizerStrings' {
+  const strings: ICssExtensionApplicationCustomizerStrings;
+  export = strings;
+}
